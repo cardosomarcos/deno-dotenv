d05b155e4afb306e1c8d07c2d9094c53db808143
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-01-08 15:37:04 -0200

    fix assertion with assertEqual and add parse function unit tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,7 +4,7 @@ import { removeSync, writeFileSync, env } from "deno";
 import dotenv, { parse } from "../index.ts";
 
 // @ts-ignore
-import { test, equal } from "https://deno.land/x/testing/mod.ts";
+import { test, assertEqual as equal } from "https://deno.land/x/testing/mod.ts";
 
 test({
   name: "[API consistence]: Module default exports dotenv function",
@@ -35,8 +35,8 @@ test({
   name: "[dotenv function]: Read .env, parse to vars and return it",
   fn () {
     const value = "Hari Seldon";
-    const vars = dotenvFrom(`A = "${value}"`);
-    equal(vars.A, value);
+    const vars = dotenvFrom(`A1 = "${value}"`);
+    equal(vars.A1, value);
   }
 });
 
@@ -47,40 +47,95 @@ test({
     const valueA = "Hari Seldon";
     const valueB = "Stor Gendibal";
 
-    dotenvFrom(`A = "${valueA}"`);
-
     vars = env();
+    vars.A2 = valueA;
 
-    equal(vars.A, valueA);
-    equal(vars.B, undefined);
+    equal(vars.A2, valueA);
+    equal(vars.B2, undefined);
 
     dotenvFrom(`
-      A = "${valueB}"
-      B = "${valueB}"
+      A2 = "${valueB}"
+      B2 = "${valueB}"
     `);
 
     vars = env();
 
-    equal(vars.A, valueB);
-    equal(vars.B, valueB);
+    equal(vars.A2, valueB);
+    equal(vars.B2, valueB);
   }
 });
 
 test({
-  name: "[dotenv function]: merges .env vars with environment ones",
+  name: "[dotenv function]: Merges .env vars with environment ones",
   fn () {
     let vars;
     const valueA = "Hari Seldon";
     const valueB = "Stor Gendibal";
 
-    vars = dotenvFrom(`A = "${valueA}"`);
+    vars = dotenvFrom(`A3 = "${valueA}"`);
 
-    equal(vars.A, valueA);
-    equal(vars.B, undefined);
+    equal(vars.A3, valueA);
+    equal(vars.B3, undefined);
 
-    vars = dotenvFrom(`B = "${valueB}"`);
+    vars = dotenvFrom(`B3 = "${valueB}"`);
 
-    equal(vars.A, valueA);
-    equal(vars.B, valueB);
+    equal(vars.A3, valueA);
+    equal(vars.B3, valueB);
+  }
+});
+
+test({
+  name: "[parse function]: Resolve spaces between var, equals symbol and value",
+  fn () {
+    const value = "Hari Seldon";
+    const vars = parse(`
+      A4   =  ${value}
+      B4=${value}
+          C4\t \t \t=\t  \t"${value}"
+    `);
+    equal(vars.A4, value);
+    equal(vars.B4, value);
+    equal(vars.C4, value);
+  }
+});
+
+test({
+  name: "[parse function]: Reads only one var per line",
+  fn () {
+    const value = "Hari Seldon";
+    const vars = parse(`
+      A5 = "${value}"; B5 = "${value}"
+      C5 = ${value}
+      D5 =
+      E5 = "
+           ${value}"
+    `);
+    equal(vars.A5, `${value}"; B5 = "${value}`);
+    equal(vars.B5, undefined);
+    equal(vars.C5, value);
+    equal(vars.D5, '');
+    equal(vars.E5, '"');
+  }
+});
+
+test({
+  name: "[parse function]: Parses \\n, empty values as empty string and double quotes",
+  fn () {
+    const vars = parse(`
+      A6 = "\\nA\\nE\\nI"
+      B6 =
+      C6 = "    "
+      D6 = \\n
+      E6 = "\\n"
+      F6 = "
+      F7 = { "name": "Hari Seldon" }
+    `);
+    equal(vars.A6, '\nA\nE\nI');
+    equal(vars.B6, '');
+    equal(vars.C6, '    ');
+    equal(vars.D6, '\\n');
+    equal(vars.E6, '\n');
+    equal(vars.F6, '"');
+    equal(vars.F7, '{ "name": "Hari Seldon" }');
   }
 });
