597a95de2f9c500193999bf88144497abb90d504
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-10-31 23:44:54 -0300

    Use Record on DotenvVariables type

--- index.ts
+++ index.ts
@@ -3,9 +3,7 @@ import { env, readFileSync } from "deno";
 /**
  * Variables defined on .env file or environment.
  */
-export interface DotenvVariables {
-  [name: string]: string;
-}
+export type DotenvVariables = Record<string, string>;
 
 const LINE_BREAK = /\r\n|\n|\r/;
 const DECLARATION = /^\s*(\w+)\s*\=\s*(.*)?\s*$/;
@@ -17,14 +15,14 @@ const DECLARATION = /^\s*(\w+)\s*\=\s*(.*)?\s*$/;
  * //=> { NAME: 'Hari Seldon', NICK: 'Seldon' }
  * @param source - Source of a `.env` file.
  */
-export function parse (source: string): DotenvVariables {
+export function parse(source: string): DotenvVariables {
   const lines = source.split(LINE_BREAK);
 
   return lines.reduce((vars: DotenvVariables, line: string) => {
     if (!DECLARATION.test(line))
       return vars;
 
-    const [ , name, value ] = DECLARATION.exec(line)!;
+    const [, name, value] = DECLARATION.exec(line)!;
 
     if (!value)
       vars[name] = "";
@@ -53,7 +51,7 @@ export interface DotenvOptions {
  * const env = dotenv();
  * db.connect(env.DB_USERNAME, env.DB_PASSWORD);
  */
-export default function dotenv ({ path = '.env' }: DotenvOptions = {}): DotenvVariables {
+export default function dotenv({ path = '.env' }: DotenvOptions = {}): DotenvVariables {
   const file = readFileSync(path);
   const vars = parse(decoder.decode(file));
   return Object.assign(env(), vars);
